38e2f33a7577a6f1582a2a4aee47b07937814212
raphaelrieuhelft <raphael.rieuhelft@gmail.com>
2015-01-24 17:55:14 +0100

    Corrections doc et lexer

--- shared_memory.ml
+++ shared_memory.ml
@@ -1,5 +1,22 @@
  let microprocessor_inputs = Array.make Names.ninputs false
  
+ let m_in = Mutex.create();
+ let m_out = Mutex.create();
+ let c = Condition.create();
+ 
+ let read_inputs (f: unit->unit) = 
+	Mutex.lock m_in;
+	f();
+	Mutex.unlock m_in;
+ 
+ let change_output (f: unit ->unit) =
+	Mutex.lock m_out;
+	f();
+	Condition.signal c;
+	Mutex.unlock m_out
+ 
+ 
+ 
  let digits_RAM_count = 16
  let digits_RAM_word_size = 7
  let digits_RAM = Array.make digits_RAM_count [||]
