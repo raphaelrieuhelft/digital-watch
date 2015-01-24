4db9d593420527f92db791472f54cb088151e341
projcompil <nissim.zerbib@ens.fr>
2015-01-24 21:04:12 +0100

    continuation intepreteur

--- compilateur_interpreteur/interpreteur.ml
+++ compilateur_interpreteur/interpreteur.ml
@@ -8,13 +8,20 @@ let regs = Array.make 32 0
 
 let inv_tbl = Array.map (fun (s,t) -> t) Lexer.reg_tbl
 
-let h_reg =
-    let affiche regs = Array.iter (fun x -> print_string (inv_tbl.(i) ^" = ") ; print_int x ; print string ", ") regs ; print_newline ()
+let affiche regs = Array.iter (fun x -> print_string (inv_tbl.(i) ^" = ") ; print_int x ; print string ", ") regs ; print_newline ()
+let afficher reste = Array.iter (fun x -> print_int x ; print string ", ") reste
+
+let nimm = Array.make 8 1
+
+let ramaff = Array.make 16 0
 
 let traite t =
     let n = Array.length t in
     let rec aux i =
         affiche regs ;
+        afficher nimm ;
+        affichier ramaff ;
+        print_newline() ;
         if i < n then begin match t.(i) with
             | Ivide -> aux (i+1)
             | Icbeq(x,y) -> if regs.(x) = regs.(y) then aux (i+2) else aux(i+1)
@@ -23,9 +30,9 @@ let traite t =
             | Iincr (x,y) -> t.(x) <- t.(y) + 1 ; aux(i+1)
             | Imodf (x,y) -> t.(x) <- t.(y) mod 4 ; aux(i+1)
             | Ilbi (y)  -> t.(31) <- y ; aux(i+1)
-            | Ilin(x,y) -> ()
-            | Iso(x,y) ->()
-            | Isd(x,y) ->()
+            | Ilin(x,y) -> t.(x) <- nimm.(y)
+            | Iso(x,y) -> ramaff.(y) <- t.(x)
+            | Isd(x,y) -> ramaff.(y) <- t.(x)
         end;
 
     in aux 0
