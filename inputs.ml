71fb45d708e8929439d64b8ddab5266f16af48aa
dianegalloiswong <diane.gallois-wong@ens.fr>
2015-01-25 13:33:45 +0100

    Ajoute des fonctions dans inputs, non implémentées

--- inputs.ml
+++ inputs.ml
@@ -1,6 +1,18 @@
 open Graphics
 open Shared_memory
 
+let force_pause () = failwith "non fait"
+let force_marche () = failwith "non fait"
+
+let reset () =
+	force_pause ();
+	failwith "non fait"
+	
+let change_reglage_courant () = failwith "non fait"
+let incremente_reglage_courant () = failwith "non fait"
+
+
+
 let on_input c = match c with
   |'p'|'\r' -> switch_input 1
   |'\t' -> switch_input 2
