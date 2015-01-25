5e95b650278218e18fa532c5cd61aab6dbefc3bc
projcompil <nissim.zerbib@ens.fr>
2015-01-25 19:37:46 +0100

    début synchro

---
+++ synchro.ml
@@ -0,0 +1,16 @@
+open Inputs
+
+let demande_temps () = Unix.localtime (Unix.gettimeofday())
+
+let rec repete f n = match n with
+    | 0 -> ()
+    | n -> f() ; repete f (n-1)
+
+let synchro () =
+    let t = demande_temps () in
+        reset();
+        List.iter (fun n -> repete incremente_reglage_courant n ; change_reglage_courant) [t.Unix.tm_sec ; t.tm_min + 1 ; t.tm_hour ; t.tm_mday ; t.tm_mon ; t.tm_year - 100 ] ;
+        let t2 = demande_temps () in
+            Unix.wait (60 - (t2.Unix.tm_sec - t.Unix.tm_sec + 60 * (t2.tm_min - t1.tm_min))) ;
+            force_marche() ;
+
